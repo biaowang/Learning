6ff37e69ed720021cb3700d5c76d62de43e5836c
biaowang <ibiaowang@gmail.com>
2016-11-08 10:52:53 +0800

    [Biao] Add Hamcrest as test assertion

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,6 +1,9 @@
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class SimpleTest {
 
     @BeforeClass
@@ -11,6 +14,7 @@ public class SimpleTest {
     @Test
     public void aTest() {
         System.out.println("run a test by testNG");
+        assertThat("1 + 1 = 2", 2, equalTo(1 + 1));
     }
 
 }
