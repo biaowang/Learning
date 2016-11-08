ef1f4c8de6dd9fe1c184c4c4e7e301c6553b6544
biaowang <ibiaowang@gmail.com>
2016-11-08 18:22:41 +0800

    [Biao] Seperate Test data and logic

--- src/test/java/SimpleUITest.java
+++ src/test/java/SimpleUITest.java
@@ -13,28 +13,30 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class SimpleUITest {
 
+    private String phantomJSDriverPath = "Tools/Mac/phantomjs";
+
     private static final long TIME_OUT_IN_SECONDS = 10;
     private WebDriver driver;
     private WebDriverWait wait;
-
+    private String testURL = "https://www.baidu.com/";
+    private String SEARCH_TEXTBOX_ID = "kw";
+    private String searchKeyword = "test";
+    private String SEARCH_BUTTON_ID = "su";
 
     @BeforeClass
     public void setUp() {
-        System.setProperty("phantomjs.binary.path", "Tools/Mac/phantomjs");
+        System.setProperty("phantomjs.binary.path", phantomJSDriverPath);
         driver = new PhantomJSDriver();
         wait = new WebDriverWait(driver, TIME_OUT_IN_SECONDS);
     }
 
-
     @Test
     public void aWebTest() throws InterruptedException {
-        driver.get("https://www.baidu.com/");
-        driver.findElement(By.id("kw")).sendKeys("test");
-        driver.findElement(By.id("su")).click();
-        waitForPageLoad();
-        wait.until(ExpectedConditions.titleContains("test"));
-        System.out.println(driver.getTitle());
-        assertThat("Result page contains search keyword", driver.getTitle().contains("test"));
+        driver.get(testURL);
+        driver.findElement(By.id(SEARCH_TEXTBOX_ID)).sendKeys(searchKeyword);
+        driver.findElement(By.id(SEARCH_BUTTON_ID)).click();
+        wait.until(ExpectedConditions.titleContains(searchKeyword));
+        assertThat("Result page contains search keyword", driver.getTitle().contains(searchKeyword));
     }
 
 
