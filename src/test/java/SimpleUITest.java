702eb7dc59e1a79f2059fb35cbd2a1b93dc90861
biaowang <ibiaowang@gmail.com>
2016-11-09 10:11:21 +0800

    [Biao] Create TestPage class and move page properties in it

--- src/test/java/SimpleUITest.java
+++ src/test/java/SimpleUITest.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import pages.BaiduHomePage;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -15,28 +16,23 @@ public class SimpleUITest {
 
     private String phantomJSDriverPath = "Tools/Mac/phantomjs";
 
-    private static final long TIME_OUT_IN_SECONDS = 10;
     private WebDriver driver;
     private WebDriverWait wait;
-    private String testURL = "https://www.baidu.com/";
-    private String SEARCH_TEXTBOX_ID = "kw";
-    private String searchKeyword = "test";
-    private String SEARCH_BUTTON_ID = "su";
 
     @BeforeClass
     public void setUp() {
         System.setProperty("phantomjs.binary.path", phantomJSDriverPath);
         driver = new PhantomJSDriver();
-        wait = new WebDriverWait(driver, TIME_OUT_IN_SECONDS);
+        wait = new WebDriverWait(driver, BaiduHomePage.TIME_OUT_IN_SECONDS);
     }
 
     @Test
     public void aWebTest() throws InterruptedException {
-        driver.get(testURL);
-        driver.findElement(By.id(SEARCH_TEXTBOX_ID)).sendKeys(searchKeyword);
-        driver.findElement(By.id(SEARCH_BUTTON_ID)).click();
-        wait.until(ExpectedConditions.titleContains(searchKeyword));
-        assertThat("Result page contains search keyword", driver.getTitle().contains(searchKeyword));
+        driver.get(BaiduHomePage.testURL);
+        driver.findElement(By.id(BaiduHomePage.SEARCH_TEXTBOX_ID)).sendKeys(BaiduHomePage.searchKeyword);
+        driver.findElement(By.id(BaiduHomePage.SEARCH_BUTTON_ID)).click();
+        wait.until(ExpectedConditions.titleContains(BaiduHomePage.searchKeyword));
+        assertThat("Result page contains search keyword", driver.getTitle().contains(BaiduHomePage.searchKeyword));
     }
 
 
---
+++ src/test/java/pages/BaiduHomePage.java
@@ -0,0 +1,11 @@
+package pages;
+
+public class BaiduHomePage {
+
+    public static final long TIME_OUT_IN_SECONDS = 10;
+    public static String testURL = "https://www.baidu.com/";
+    public static String SEARCH_TEXTBOX_ID = "kw";
+    public static String searchKeyword = "test";
+    public static String SEARCH_BUTTON_ID = "su";
+
+}
